9c5f556093d653a2984fcdf96a08f231321cbaf1
w1llow1sp <mari.mokeeva.98@list.ru>
2022-06-18 13:26:26 +0500

    Подготовили проект, сделали шапку и футер

---
+++ src/layout/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <nav className=" blue-grey darken-2">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          w1llow1sp Movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="#!">Repo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Header };
---
+++ src/layout/Main.jsx
@@ -0,0 +1,5 @@
+function Main() {
+  return <main className="container content">Hello from jsx</main>;
+}
+
+export { Main };
