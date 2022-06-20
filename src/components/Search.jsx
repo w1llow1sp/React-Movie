bb9c6f8245bd25fe1d16ce37165eaf34050c4873
w1llow1sp <mari.74.front@gmail.com>
2022-06-20 16:55:20 +0500

    refactored code in Search.jsx and Main.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,97 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Search extends React.Component {
-    state = {
-        search: '',
-        type: 'all',
-    };
+const Search = (props) => {
+    const { searchMovies = Function.prototype } = props;
+
+    const [search, setSearch] = useState('');
+    const [type, setType] = useState('all');
 
     //
 
-    handleKey = (e) => {
+    const handleKey = (e) => {
         if (e.key === 'Enter') {
-            this.props.searchMovies(this.state.search, this.state.type);
+            searchMovies(search, type);
         }
     };
 
     //Добавим фильтрацию по типу
 
-    handleFilter = (e) => {
-        this.setState(
-            () => ({ type: e.target.dataset.type }),
-            () => {
-                this.props.searchMovies(this.state.search, this.state.type);
-            }
-        );
+    const handleFilter = (e) => {
+        setType(e.target.dataset.type);
+        searchMovies(search, e.target.dataset.type);
     };
 
-    render() {
-        return (
-            <div className='row'>
-                <div className='input-field'>
-                    <input
-                        className='validate'
-                        placeholder='Search'
-                        type='search'
-                        value={this.state.search}
-                        onChange={(event) =>
-                            this.setState({ search: event.target.value })
-                        }
-                        onKeyDown={this.handleKey}
-                    />
-                    <button
-                        className='btn search-btn blue-grey darken-4'
-                        onClick={() =>
-                            this.props.searchMovies(
-                                this.state.search,
-                                this.state.type
-                            )
-                        }
-                    >
-                        <i className='small material-icons '>search</i>
-                        search
-                    </button>
-                </div>
-                <div>
-                    <p>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='type'
-                                type='radio'
-                                data-type='all'
-                                onChange={this.handleFilter}
-                                checked={this.state.type === 'all'}
-                            />
-                            <span>All</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='type'
-                                type='radio'
-                                data-type='movie'
-                                onChange={this.handleFilter}
-                                checked={this.state.type === 'movie'}
-                            />
-                            <span>Movies Only</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='type'
-                                type='radio'
-                                data-type='series'
-                                onChange={this.handleFilter}
-                                checked={this.state.type === 'series'}
-                            />
-                            <span>Series Only</span>
-                        </label>
-                    </p>
-                </div>
+    return (
+        <div className='row'>
+            <div className='input-field'>
+                <input
+                    className='validate'
+                    placeholder='Search'
+                    type='search'
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                    onKeyDown={handleKey}
+                />
+                <button
+                    className='btn search-btn blue-grey darken-4'
+                    onClick={() => searchMovies(search, type)}
+                >
+                    <i className='small material-icons '>search</i>
+                    search
+                </button>
             </div>
-        );
-    }
-}
+            <div>
+                <p>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='all'
+                            onChange={handleFilter}
+                            checked={type === 'all'}
+                        />
+                        <span>All</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='movie'
+                            onChange={handleFilter}
+                            checked={type === 'movie'}
+                        />
+                        <span>Movies Only</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='series'
+                            onChange={handleFilter}
+                            checked={type === 'series'}
+                        />
+                        <span>Series Only</span>
+                    </label>
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,53 +1,53 @@
 // На этом уровне мы будем получать фильмы
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
-    state = {
-        movies: [],
-        loading: true,
-    };
+function Main() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=blade`)
-            .then((response) => response.json())
-            .then((data) =>
-                this.setState({ movies: data.Search, loading: false })
-            );
-    }
-
-    // Функция для обновления State и отрисовке фильмов по запросу
-
-    searchMovies = (str, type = 'all') => {
-        this.setState({ loading: true });
+    const searchMovies = (str, type = 'all') => {
+        setLoading(true);
         fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
             .then((response) => response.json())
-            .then((data) =>
-                this.setState({ movies: data.Search, loading: false })
-            );
+            .then((data) => {
+                setLoading(false);
+                setMovies(data.Search);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            });
     };
 
-    render() {
-        // Делаем прелоадер и деструкторизацию
+    useEffect(() => {
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=blade`)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            });
+    }, []);
 
-        const { movies, loading } = this.state;
-
-        return (
-            <main className='container content'>
-                <Search searchMovies={this.searchMovies} />
-                {loading ? <Preloader /> : <Movies movies={movies} />}
-            </main>
-        );
-    }
+    return (
+        <main className='container content'>
+            <Search searchMovies={searchMovies} />
+            {loading ? <Preloader /> : <Movies movies={movies} />}
+        </main>
+    );
 }
 
 export { Main };
